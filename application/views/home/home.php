4f3317bdda232ac4a9219cab409f406024530f75
Micol Peralta <linettekill1@gmail.com>
2018-11-24 03:00:44 +0100

    Initial commit

---
+++ application/views/home/home.php
@@ -0,0 +1,300 @@
+<div class="pt-5" style="background-image:url(https://www.tbo.com/storyimage/TB/20140416/ARTICLE/140419373/AR/0/AR-140419373.jpg);min-height: 600px ">
+    <div class="container pt-5">
+        <div class="row pt-5">
+          	<div class="col-lg-8 text-white pr-5">
+          		<h1 class="display-4 font-weight-bold">Estimula el aprendizaje de tu bebe donde quieras</h1>
+		        <p class="pt-4 display-6">El momento perfecto para aprender</p>
+		        <p class="pt-4 display-6"><i class="fas fa-play-circle"></i>&nbsp; <a href="#video-home-section">Ver video de como funciona &raquo;</a></p>
+          	</div>
+          	<div class="col-lg-4">
+          		<div class="container">
+          			<form class="text-center bg-white p-5">
+						<h5 class="display-6 font-weight-bold pb-3 text-bgreen">¡Contáctanos!</h5>
+						<div class="form-group">
+					    	<input type="email" class="form-control input-pink" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Correo">
+					    
+					  	</div>
+					  	<div class="form-group">
+					    	<select class="form-control input-pink">
+								<option>Edad del bebé</option>
+							</select>
+					  	</div>
+					  	<p class="text-left small"><i class="fas fa-lock"></i>&nbsp; Libre de spam, EMET se compromete a brindarte un contenido único para el aprendizaje de tu bebé.</p>
+					  	<button type="submit" class="btn btn-r btn-yellow p-1 btn-lg btn-block">Comprar</button>
+					</form>
+          		</div>
+          	</div>
+        </div>
+    </div>
+</div>
+<div id="video-home-section">
+</div>
+
+<div class="kit-home-section text-center pt-5 pb-5">
+	<a href="#" class="btn btn-r btn-light-blue text-white btn-lg pr-5 pl-5">Adquiere el kit</a>
+</div>
+
+<div class="container text-center p-5 pr-md-0 pl-md-0">
+	<img class='pt-5' src="<?php echo base_url('assets/images/emet-logo.png') ?>" width='30%'>
+	<p class="pt-5 pl-5 pr-5 lead">
+		Es un método de aprendizaje simple que te ofrece una serie de pasos utilizando la "educación musical temprana" ayudandote a identificar patologias en tu bebé para que acudas al especialista a verificar sospechas. Juntos tendremos la oportunidad ideal para que tu y tu bebé tengan siempre ese espacio a solas de diversión.
+	</p>
+
+	<a class="display-6" href="">Leer más del metodo</a>
+
+</div>
+
+<div class="bg-red mt-5 mb-5">
+	<div class="container">
+		<div class="row align-items-center pt-5 pb-5">
+			<div class="col-lg-3 col-md-3 pt-4 pb-4 pr-5 pl-5">
+				<p class="display-6 border-right text-right pr-3 text-white">Datos importantes</p>
+			</div>
+			<div class="col-lg-3 col-md-3 pt-4 pb-4">
+				<div class="row pt-3 pb-3 text-white">
+					<div class="col-lg-4">
+						<p><i class="pt-1 fas fa-drum display-5"></i></p>
+					</div>
+					<div class="col-lg-8">
+						<p class="display-5 ">95% +</p>
+						<small class="">Del aprendizaje efectivo es logrado a travez de la estimulación temprana musical.</small>
+					</div>
+				</div>
+			</div>
+			<div class="col-lg-3 col-md-3 pt-4 pb-4">
+				<div class="row pt-3 pb-3 text-white">
+					<div class="col-lg-4">
+						<p><i class="pt-1 fas fa-clock display-5"></i></p>
+					</div>
+					<div class="col-lg-8">
+						<p class="display-5 ">30% +</p>
+						<small class="">Ahorra más tiempo y dinero acelerando el aprendizaje de tu bebé.</small>
+					</div>
+				</div>
+			</div>
+			<div class="col-lg-3 col-md-3 pt-4 pb-4">
+				<div class="row pt-3 pb-3 text-white">
+					<div class="col-lg-4">
+						<p><i class="pt-1 fas fa-play-circle display-5"></i></p>
+					</div>
+					<div class="col-lg-8">
+						<p class="display-5 ">95% +</p>
+						<small class="">Del aprendizaje efectivo es logrado a travez de la estimulación temprana musical.</small>
+					</div>
+				</div>
+			</div>
+			</div>
+	</div>
+</div>
+
+<div class="container pt-5 text-center">
+	<p class="display-4 font-weight-bold text-red mt-5">¿Cómo funciona EMET?</p>
+	<div class="row mt-5">
+		<div class="col-lg-4 mt-5">
+			<img src="<?php echo base_url('assets/images/number-1.png') ?>" width='70px'>
+			<p class="text-primary display-6 pt-4">Regístrate</p>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad iure neque architecto! Voluptatem ea eveniet laborum minima ipsam.</p>
+		</div>
+		<div class="col-lg-4 mt-5">
+			<img src="<?php echo base_url('assets/images/number-2.png') ?>" width='70px'>
+			<p class="text-primary display-6 pt-4">Adquiere tu Kit</p>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad iure neque architecto! Voluptatem ea eveniet laborum minima ipsam.</p>
+		</div>
+		<div class="col-lg-4 mt-5">
+			<img src="<?php echo base_url('assets/images/number-3.png') ?>" width='70px'>
+			<p class="text-primary display-6 pt-4">Comienza las lecciones</p>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad iure neque architecto! Voluptatem ea eveniet laborum minima ipsam.</p>
+		</div>
+
+	</div>
+	
+	<div class="mt-5 pt-5 mb-5 pb-5">
+		<a href="#" class="btn btn-r btn-yellow text-white btn-lg pr-5 pl-5 display-6">Solicitar Kit</a>
+	</div>
+	
+</div>
+
+<div id="carouselInfo" class="carousel slide slideInfo" data-ride="carousel">
+	<ol class="carousel-indicators">
+	    <li data-target="#carouselInfo" data-slide-to="0" class="active"></li>
+	    <li data-target="#carouselInfo" data-slide-to="1"></li>
+	    <li data-target="#carouselInfo" data-slide-to="2"></li>
+	</ol>
+	<div class="carousel-inner">
+	   <div class="container">
+	   	 <div class="carousel-item active slide slideInfo">
+		    <p class="display-4 font-weight-bold mb-5">Conoce más sobre la estimulación</p>
+
+		    <div class="mt-5 pt-5">
+		    	<p class="display-6">
+			    	Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque, quos, animi. Officia, expedita perspiciatis eius quasi cumque ad nobis, vero saepe dolore. Nemo enim architecto, sunt dicta blanditiis ipsam quia!
+			    </p>
+			    <p class="display-6">
+			    	Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque, quos, animi. Officia, expedita perspiciatis eius quasi cumque ad nobis, vero saepe dolore. Nemo enim architecto, sunt dicta blanditiis ipsam quia!
+			    </p>
+		    </div>
+	    </div>
+	    <div class="carousel-item slide slideInfo">
+		    <p class="display-4 font-weight-bold mb-5">Conoce más sobre la estimulación</p>
+
+		    <div class="mt-5 pt-5">
+		    	<p class="display-6">
+			    	Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque, quos, animi. Officia, expedita perspiciatis eius quasi cumque ad nobis, vero saepe dolore. Nemo enim architecto, sunt dicta blanditiis ipsam quia!
+			    </p>
+			    <p class="display-6">
+			    	Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque, quos, animi. Officia, expedita perspiciatis eius quasi cumque ad nobis, vero saepe dolore. Nemo enim architecto, sunt dicta blanditiis ipsam quia!
+			    </p>
+		    </div>
+	    </div>
+	    <div class="carousel-item slide slideInfo">
+		    <p class="display-4 font-weight-bold mb-5">Conoce más sobre la estimulación</p>
+
+		    <div class="mt-5 pt-5">
+		    	<p class="display-6">
+			    	Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque, quos, animi. Officia, expedita perspiciatis eius quasi cumque ad nobis, vero saepe dolore. Nemo enim architecto, sunt dicta blanditiis ipsam quia!
+			    </p>
+			    <p class="display-6">
+			    	Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque, quos, animi. Officia, expedita perspiciatis eius quasi cumque ad nobis, vero saepe dolore. Nemo enim architecto, sunt dicta blanditiis ipsam quia!
+			    </p>
+		    </div>
+	    </div>
+	   </div>
+	</div>
+	
+</div>
+
+
+<div id="carouselExpertos" class="carousel slide slideExperto" data-ride="carousel">
+	<ol class="carousel-indicators">
+	    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+	    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+	    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+	</ol>
+	<div class="carousel-inner">
+	   	<div class="carousel-item active slide">
+	   	 	<div class="container">
+	   	 		<div class="row align-items-center pt-5 pb-5">
+		   	 		<div class="col-lg-4 text-center pt-5 pb-5">
+		   	 			<img  src="<?php echo base_url('assets/images/user-icon.png') ?>" width="50%">
+		   	 			<p class="display-6 mt-4 text-red font-weight-bold">Gisset Polanco</p>
+		   	 			<p class="display-6 font-weight-bold">Co-Founder Emet</p>
+		   	 		</div>
+		   	 		<div class="col-lg-8 pt-5 pb-5">
+		   	 			<p class="display-4 font-weight-bold mb-5 text-red">Equipo de expertos</p>
+
+		   	 			<p class="display-6 text-secondary">
+				    	Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque, quos, animi. Officia, expedita perspiciatis eius quasi cumque ad nobis, vero saepe dolore. Nemo enim architecto, sunt dicta blanditiis ipsam quia!</p>
+		   	 		</div>
+		   	 	</div>
+	   	 	</div>
+	    </div>
+	    <div class="carousel-item slide">
+	   	 	<div class="container">
+	   	 		<div class="row align-items-center pt-5 pb-5">
+		   	 		<div class="col-lg-4 text-center pt-5 pb-5">
+		   	 			<img  src="<?php echo base_url('assets/images/user-icon.png') ?>" width="50%">
+		   	 			<p class="display-6 mt-4 text-red font-weight-bold">Gisset Polanco</p>
+		   	 			<p class="display-6 font-weight-bold">Co-Founder Emet</p>
+		   	 		</div>
+		   	 		<div class="col-lg-8 pt-5 pb-5">
+		   	 			<p class="display-4 font-weight-bold mb-5 text-red">Equipo de expertos</p>
+
+		   	 			<p class="display-6 text-secondary">
+				    	Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque, quos, animi. Officia, expedita perspiciatis eius quasi cumque ad nobis, vero saepe dolore. Nemo enim architecto, sunt dicta blanditiis ipsam quia!</p>
+		   	 		</div>
+		   	 	</div>
+	   	 	</div>
+	    </div>
+	    <div class="carousel-item slide">
+	   	 	<div class="container">
+	   	 		<div class="row align-items-center pt-5 pb-5">
+		   	 		<div class="col-lg-4 text-center pt-5 pb-5">
+		   	 			<img  src="<?php echo base_url('assets/images/user-icon.png') ?>" width="50%">
+		   	 			<p class="display-6 mt-4 text-red font-weight-bold">Gisset Polanco</p>
+		   	 			<p class="display-6 font-weight-bold">Co-Founder Emet</p>
+		   	 		</div>
+		   	 		<div class="col-lg-8 pt-5 pb-5">
+		   	 			<p class="display-4 font-weight-bold mb-5 text-red">Equipo de expertos</p>
+
+		   	 			<p class="display-6 text-secondary">
+				    	Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque, quos, animi. Officia, expedita perspiciatis eius quasi cumque ad nobis, vero saepe dolore. Nemo enim architecto, sunt dicta blanditiis ipsam quia!</p>
+		   	 		</div>
+		   	 	</div>
+	   	 	</div>
+	    </div>
+	</div>
+	<a class="carousel-control-prev" href="#carouselExpertos" role="button" data-slide="prev">
+		<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+		<span class="sr-only">Previous</span>
+	</a>
+	<a class="carousel-control-next" href="#carouselExpertos" role="button" data-slide="next">
+	    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+	    <span class="sr-only">Next</span>
+	</a>
+</div>
+
+
+<div class="container p-5">
+	<div class="row p-5">
+		<div class="col-lg-4 p-5 text-center">
+			<img src="<?php echo base_url('assets/images/kit.png') ?>" alt="KIT" width="100%"></div>
+		<div class="col-lg-8 p-5">
+			<p class="display-5 font-weight-bold text-blue">Kit de inicio</p>
+			<p class="display-6">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem, ipsa?</p>
+			<div class="form-check form-check-inline">
+			  <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio1" value="option1 pr-3">
+			  <label class="form-check-label display-6" for="inlineRadio1">Incluír Kit</label>
+			</div>
+			<div class="form-check form-check-inline">
+			  <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio2" value="option2 pr-3">
+			  <label class="form-check-label display-6" for="inlineRadio2">Ya tengo un Kit</label>
+			</div>
+		</div>
+	</div>
+	<p class="display-5 text-red text-center font-weight-bold">Planes</p>
+	<div class="row pt-5">
+		<div class="col-lg-4 col-md-4 card-plans pt-5 pr-3 pl-3">
+			<p class="text-info font-weight-bold title-plans text-center">MENSUAL</p>
+			<p class="text-center"><span class="pt-2">US$ </span><span class="display-5 font-weight-bold pt-2">2.99</span></p>
+
+			<ul class="list-unstyled pt-5 pr-3 pl-3 mb-4">
+				<li class="pt-3 pb-3 border-top border-info text-info"><i class="fas fa-check-circle pr-5 display-6 border-info"></i> Lecciones</li>
+				<li class="pt-3 pb-3 border-top border-info text-info"><i class="fas fa-check-circle pr-5 display-6"></i> Canciones</li>
+				<li class="pt-3 pb-3 border-top border-bottom border-info text-info"><i class="fas fa-check-circle pr-5 display-6"></i> Cuentos</li>
+			</ul>
+			<div class="text-center">
+				<a href="#" class="btn btn-r btn-block btn-yellow text-dark font-weight-bold btn-lg pr-5 pl-5 mb-5 mt-5">Comprar</a>
+			</div>
+		</div>
+		<div class="col-lg-4 col-md-4 card-plan-middle pt-5">
+			<p class="text-yellow text-center font-weight-bold display-6">6 MESES</p>
+			<p class="text-center text-white"><span class="pt-2 display-6">US$ </span><span class="display-4 font-weight-bold pt-2">15.99</span></p>
+			<p class="text-center text-white">15% Desc.</p>
+
+			<ul class="list-unstyled pt-5 pr-3 pl-3 mb-4">
+				<li class="pt-3 pb-3 border-top border-white text-white"><i class="fas fa-check-circle pr-5 display-6 border-white"></i> Lecciones</li>
+				<li class="pt-3 pb-3 border-top border-white text-white"><i class="fas fa-check-circle pr-5 display-6"></i> Canciones</li>
+				<li class="pt-3 pb-3 border-top border-bottom border-white text-white"><i class="fas fa-check-circle pr-5 display-6"></i> Cuentos</li>
+			</ul>
+			<div class="text-center">
+				<a href="#" class="btn btn-r btn-block btn-yellow text-dark font-weight-bold btn-lg pr-5 pl-5 mb-5 mt-5">Comprar</a>
+			</div>
+		</div>
+		<div class="col-lg-4 col-md-4 card-plans pt-5 pr-3 pl-3">
+			<p class="text-info font-weight-bold title-plans text-center">ANUAL</p>
+			<p class="text-center"><span class="pt-2">US$ </span><span class="display-5 font-weight-bold pt-2">2.99</span></p>
+
+			<ul class="list-unstyled pt-5 pr-3 pl-3 mb-4">
+				<li class="pt-3 pb-3 border-top border-info text-info"><i class="fas fa-check-circle pr-5 display-6 border-info"></i> Lecciones</li>
+				<li class="pt-3 pb-3 border-top border-info text-info"><i class="fas fa-check-circle pr-5 display-6"></i> Canciones</li>
+				<li class="pt-3 pb-3 border-top border-bottom border-info text-info"><i class="fas fa-check-circle pr-5 display-6"></i> Cuentos</li>
+			</ul>
+			<div class="text-center">
+				<a href="#" class="btn btn-r btn-block btn-yellow text-dark font-weight-bold btn-lg pr-5 pl-5 mb-5 mt-5">Comprar</a>
+			</div>
+		</div>
+	</div>
+</div>
+
+
+
