a56e78efef9c34578fd318228409ebd0c61fa4ac
Micol Peralta <linettekill1@gmail.com>
2018-11-28 02:50:57 +0100

    Profile view

--- application/views/auth/register-profile.php
+++ application/views/auth/register-profile.php
@@ -1,58 +1,55 @@
 <div class="container mt-5">
-	<div class="row align-items-center pt-5" id="ParentsProfile">
-		<div class="col-lg-4 col-md-4 pr-5">
-			<img src="<?php echo base_url('assets/images/parents.png') ?>" width='100%'>
-		</div>
-		<div class="col-lg-8 col-md-8">
-			<form>
-			  <div class="form-group">
-			    <label for="formGroupExampleInput">Nombre</label>
-			    <input type="text" class="form-control" placeholder="Ingresar nombre del tutor">
-			  </div>
-			  <div class="form-group">
-			    <label for="formGroupExampleInput">Apellido</label>
-			    <input type="text" class="form-control" placeholder="Ingresar apellido del tutor">
-			  </div>
-			  <div class="form-group">
-			    <label for="formGroupExampleInput">Fecha de nacimiento</label>
-			    <input type="date" class="form-control">
-			  </div>
-			</form>
-
-		</div>
-
+	<form action="">
+		<div class="row align-items-center pt-5" id="ParentsProfile">
+			<div class="col-lg-4 col-md-4 pr-5">
+				<img src="<?php echo base_url('assets/images/parents.png') ?>" width='100%'>
+			</div>
+			<div class="col-lg-8 col-md-8">
+				  <div class="form-group">
+				    <label class="text-dblue font-weight-bold">Nombre</label>
+				    <input type="text" class="form-control" placeholder="Ingresar nombre del tutor" required>
+				  </div>
+				  <div class="form-group">
+				    <label class="text-dblue font-weight-bold">Apellido</label>
+				    <input type="text" class="form-control" placeholder="Ingresar apellido del tutor" required>
+				  </div>
+				  <div class="form-group">
+				    <label class="text-dblue font-weight-bold">Fecha de nacimiento</label>
+				    <input type="date" class="form-control" required>
+				  </div>
+			</div>
 	</div>
 	<a href="#" class="btn btn-light-blue text-white btn-block mt-5" id="ShowBabyProfile">Continuar</a>
-	<div class="row align-items-center pt-5" id="BabyProfile">
+	<div class="row align-items-center d-none pt-5" id="BabyProfile">
 		<div class="col-lg-4 col-md-4 pr-5">
 			<img src="<?php echo base_url('assets/images/baby.png') ?>" width='100%'>
 		</div>
 		<div class="col-lg-8 col-md-8">
-			<form>
-			  <div class="form-group">
-			    <label for="formGroupExampleInput">Nombre</label>
-			    <input type="text" class="form-control" placeholder="Ingresar nombre del bebé">
-			  </div>
-			  <div class="form-group">
-			    <label for="formGroupExampleInput">Fecha de nacimiento</label>
-			    <input type="date" class="form-control">
-			  </div>
-			  <div class="form-group">
-			    <label for="formGroupExampleInput">Peso</label>
-			    <input type="number" class="form-control" placeholder="Ingresar peso del bebé">
-			  </div>
-			  <div class="form-group">
-			    <label for="formGroupExampleInput">Estatura</label>
-			    <input type="number" class="form-control" placeholder="Ingresar estatura del bebé">
-			  </div>
-			  <div class="form-group">
-			    <label for="formGroupExampleInput">Semana de gestación</label>
-			    <input type="number" class="form-control" placeholder="Ingresar en cual semana de gestación nació el bebé">
-			  </div>
+		  <div class="form-group">
+		    <label class="text-dblue font-weight-bold" for="formGroupExampleInput">Nombre</label>
+		    <input type="text" class="form-control" placeholder="Ingresar nombre del bebé" required>
+		  </div>
+		  <div class="form-group">
+		    <label class="text-dblue font-weight-bold" for="formGroupExampleInput">Fecha de nacimiento</label>
+		    <input type="date" class="form-control" max='<?php echo date('Y-m-d');?>' required>
+		  </div>
+		  <div class="form-group">
+		    <label class="text-dblue font-weight-bold" for="formGroupExampleInput">Peso</label>
+		    <input type="number" class="form-control" placeholder="Ingresar peso del bebé" required>
+		  </div>
+		  <div class="form-group">
+		    <label class="text-dblue font-weight-bold" for="formGroupExampleInput">Estatura</label>
+		    <input type="number" class="form-control" placeholder="Ingresar estatura del bebé" required>
+		  </div>
+		  <div class="form-group">
+		    <label class="text-dblue font-weight-bold" for="formGroupExampleInput">Semana de gestación</label>
+		    <input type="number" class="form-control" placeholder="Ingresar en cual semana de gestación nació el bebé" required>
+		  </div>
 
-			</form>
 
 		</div>
-
 	</div>
+
+	<a href="#" class="btn btn-light-blue text-white btn-block mt-5 d-none" id="saveProfile">Continuar</a>
+	</form>
 </div>
--- assets/js/functions.js
+++ assets/js/functions.js
@@ -3,5 +3,6 @@
 $('#ShowBabyProfile').click(function(){
 	$("#ParentsProfile").hide("slow");
 	$("#ShowBabyProfile").hide("slow");
-	$("#BabyProfile").show("slow");
+	$("#BabyProfile").removeClass('d-none');
+	$("#saveProfile").removeClass('d-none');
 });
