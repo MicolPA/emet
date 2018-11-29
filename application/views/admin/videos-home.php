22d56a7657717eb82f61ee450ae6adb6e79716ea
Micol Peralta <linettekill1@gmail.com>
2018-11-29 01:56:52 +0100

    CPanel

--- application/views/admin/videos-home.php
+++ application/views/admin/videos-home.php
@@ -8,25 +8,84 @@
 
 <div class="row">
 	<div class="col-lg-3">
-		<div class="mb-3 p-3 display-6 font-weight-bold div-bottom-blue">
-			Socio afectiva <span class="badge bg-blue text-white">45</span>
-		</div>
+		<a class='non-style-link' href="<?php echo base_url('admin/videos') ?>">
+			<div class="bg-pink text-center text-white display-6 font-weight-bold pb-1 pt-1">
+				<i class="fas fa-plus pr-2"></i>Nuevo video
+			</div>
+		</a>
 	</div>
 	<div class="col-lg-3">
-		<div class="mb-3 p-3 display-6 font-weight-bold div-bottom-pink">
-			Cognitiva <span class="badge bg-pink text-white">27</span>
-		</div>
+		<a class='non-style-link' href="<?php echo base_url('admin/videos') ?>">
+			<div class="bg-blue text-center text-white display-6 font-weight-bold pb-1 pt-1">
+				<i class="fas fa-search pr-2"></i>Buscar video
+			</div>
+		</a>
 	</div>
-	<div class="col-lg-3">
-		<div class="mb-3 p-3 display-6 font-weight-bold div-bottom-green">
-			Lenguaje <p class="badge bg-green m-0 text-right text-white">9</p>
+</div>
+
+<div class="row">
+	<div class="col-lg-8 pt-5">
+		<div class="col-lg-12 mb-5">
+			<div class="mb-3 p-3 div-bottom-red shadow">
+				<span class="display-6 font-weight-bold ">Aprendiendo a estimular</span> 
+				<p class="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsa consectetur voluptate vel corrupti, asperiores in.</p>
+				<div class="text-right">
+					<a class="btn btn-r bg-blue" href="" title="Ver"><i class="fas fa-eye text-white"></i></a>
+					<a class="btn btn-r bg-green" href="" title="Editar"><i class="fas fa-pen text-white"></i></a>
+					<a class="btn btn-r bg-red" href="" title="Editar"><i class="fas fa-trash text-white"></i></a>
+				</div>
+			</div>
 		</div>
-	</div>
-	<div class="col-lg-3">
-		<div class="mb-3 p-3 display-6 font-weight-bold div-bottom-red">
-			Motora <p class="badge bg-red m-0 text-right text-white">239</p>
+		<div class="col-lg-12 mb-5">
+			<div class="mb-3 p-3 div-bottom-green shadow">
+				<span class="display-6 font-weight-bold ">Aprendiendo a estimular</span> 
+				<p class="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsa consectetur voluptate vel corrupti, asperiores in.</p>
+				<div class="text-right">
+					<a class="btn btn-r bg-blue" href="" title="Ver"><i class="fas fa-eye text-white"></i></a>
+					<a class="btn btn-r bg-green" href="" title="Editar"><i class="fas fa-pen text-white"></i></a>
+					<a class="btn btn-r bg-red" href="" title="Editar"><i class="fas fa-trash text-white"></i></a>
+				</div>
+			</div>
 		</div>
+		<div class="col-lg-12 mb-5">
+			<div class="mb-3 p-3 div-bottom-blue shadow">
+				<span class="display-6 font-weight-bold ">Aprendiendo a estimular</span> 
+				<p class="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsa consectetur voluptate vel corrupti, asperiores in.</p>
+				<div class="text-right">
+					<a class="btn btn-r bg-blue" href="" title="Ver"><i class="fas fa-eye text-white"></i></a>
+					<a class="btn btn-r bg-green" href="" title="Editar"><i class="fas fa-pen text-white"></i></a>
+					<a class="btn btn-r bg-red" href="" title="Editar"><i class="fas fa-trash text-white"></i></a>
+				</div>
+			</div>
+		</div>
+
+
+		
 	</div>
+
+
+	<div class="col-lg-4 pt-5">
+		<div class="col-lg-12">
+			<div class="mb-3 pt-3 pb-3 pr-3 display-6 font-weight-bold div-bottom-blue">
+				Socio afectiva <span class="badge bg-blue text-white">45</span>
+			</div>
+		</div>
+		<div class="col-lg-12">
+			<div class="mb-3 pt-3 pb-3 pr-3 display-6 font-weight-bold div-bottom-pink">
+				Cognitiva <span class="badge bg-pink text-white">27</span>
+			</div>
+		</div>
+		<div class="col-lg-12">
+			<div class="mb-3 pt-3 pb-3 pr-3 display-6 font-weight-bold div-bottom-green">
+				Lenguaje <p class="badge bg-green m-0 text-right text-white">9</p>
+			</div>
+		</div>
+		<div class="col-lg-12">
+			<div class="mb-3 pt-3 pb-3 pr-3 display-6 font-weight-bold div-bottom-red">
+				Motora <p class="badge bg-red m-0 text-right text-white">239</p>
+			</div>
+		</div>
+		</div>
 	
 </div>
 
